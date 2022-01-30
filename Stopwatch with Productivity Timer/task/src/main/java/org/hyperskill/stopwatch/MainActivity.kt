c59db5bc9beef62d68cc6fa94750c1f15e707eaa
fvn70 <fvn_reg@mail.ru>
2022-01-30 11:58:25 +0700

    Stage 4/5: Custom time limit

--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
@@ -1,12 +1,14 @@
 package org.hyperskill.stopwatch
 
+import android.app.AlertDialog
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.graphics.PorterDuff
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,8 +18,10 @@ class MainActivity : AppCompatActivity() {
     val textView: TextView by lazy { findViewById(R.id.textView) }
     val startButton: Button by lazy { findViewById(R.id.startButton) }
     val resetButton: Button by lazy { findViewById(R.id.resetButton) }
+    val settingsButton: Button by lazy { findViewById(R.id.settingsButton) }
     val progressBar: ProgressBar by lazy { findViewById(R.id.progressBar) }
     var time = 0
+    var upperLimit = Int.MAX_VALUE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +31,9 @@ class MainActivity : AppCompatActivity() {
 
         val startWatch: Runnable = object : Runnable {
             override fun run() {
+                if (time >= upperLimit) {
+                    textView.setTextColor(ColorStateList.valueOf(Color.RED))
+                }
                 time++
                 if (time % 2 == 0) {
                     progressBar.indeterminateTintList = ColorStateList.valueOf(Color.CYAN)
@@ -40,6 +47,7 @@ class MainActivity : AppCompatActivity() {
 
         startButton.setOnClickListener {
             progressBar.visibility = ProgressBar.VISIBLE
+            settingsButton.isEnabled = false
             if (time == 0) {
                 thread {
                     handler.postDelayed(startWatch, 1000)
@@ -49,9 +57,24 @@ class MainActivity : AppCompatActivity() {
 
         resetButton.setOnClickListener {
             progressBar.visibility = ProgressBar.INVISIBLE
+            settingsButton.isEnabled = true
+            textView.setTextColor(ColorStateList.valueOf(Color.BLACK))
             handler.removeCallbacks(startWatch)
             time = 0
             textView.text = "00:00"
+        }
+
+        settingsButton.setOnClickListener {
+            val contentView = LayoutInflater.from(this).inflate(R.layout.dialog_main, null, false)
+            AlertDialog.Builder(this)
+                .setTitle("Set upper limit in seconds")
+                .setView(contentView)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    val editText = contentView.findViewById<EditText>(R.id.upperLimitEditText)
+                    upperLimit = editText.text.toString().toInt()
+                }
+                .setNegativeButton(android.R.string.cancel, null)
+                .show()
         }
     }
 
